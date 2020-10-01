0e16b56b0a2ceb5d24130421c9c1a6e6d2fb5dd3
triptakaku <67236570+triptakaku@users.noreply.github.com>
2020-10-01 15:34:43 +0530

    Files from AZ training Dev VM

---
+++ ps-script.ps1
@@ -0,0 +1,37 @@
+﻿#Install Azure powershell module
+Install-Module -Name az -Scope AllUsers -AllowClobber
+
+#Login to azure
+Login-AzAccount
+
+#Read the list of resource group
+Get-AzResourceGroup | Format-Table
+
+#New Resource group
+New-AzResourceGroup -Name ps-lab-grp -Location 'East US'
+
+#Declare a variable
+$tags += @{Environment="Lab"}
+Set-AzResourceGroup -ResourceGroupName ps-lab-grp -Tag $tags
+
+#Remove-AzResourceGroup -Name ps-lab-grp
+
+$appurl = "https://github.com/Azure-Samples/app-service-web-dotnet-get-started.git"
+$myurl= "https://github.com/triptakaku/myaz204repository_Tripta.git"
+
+$webappname = "mywebapp$(Get-Random)"
+$location="eastus"
+
+New-AzAppServicePlan -ResourceGroupName ps-lab-grp -Location 'East US' -Tier Free -Name ps-asp-009
+New-AzWebApp -Name $webappname -Location $location -AppServicePlan ps-asp-009 -ResourceGroupName ps-lab-grp
+
+    
+# Create deployment resource manually using ARM
+$PropertiesObject = @{
+    repoUrl = "$myurl";
+    branch = "master";
+    isManualIntegration = "true";
+}
+Set-AzResource -PropertyObject $PropertiesObject -ResourceGroupName ps-lab-grp -ResourceType Microsoft.Web/sites/sourcecontrols -ResourceName $webappname/web -ApiVersion 2015-08-01 -Force
+
+Remove-AzResourceGroup -Name ps-lab-grp
---
+++ vm-script.ps1
@@ -0,0 +1,27 @@
+﻿#Login to azure
+Login-AzAccount
+
+New-AzResourceGroup -Name AZ-204-RES -Location 'East US'
+
+New-AzResourceGroup -Name AZ-204-IAAS-GRP -Location 'East US'
+
+New-AzVM -ResourceGroupName AZ-204-IAAS-GRP -Location 'East US' -Name My-VM-01 `
+-VirtualNetworkName my-vnet-01 `
+-SubnetName webapp-subnet `
+-PublicIpAddressName my-pip-01 `
+-SecurityGroupName my-nsg-01 `
+-OpenPorts 80,3389
+
+#Install IIS
+Set-AzVMExtension -ResourceGroupName "AZ-204-IAAS-GRP" `
+-ExtensionName "IIS" `
+-VMName "My-VM-01" `
+-Location "EastUS" `
+-Publisher Microsoft.Compute `
+-ExtensionType CustomScriptExtension `
+-TypeHandlerVersion 1.8 `
+-SettingString '{"commandToExecute":"powershell Add-WindowsFeature Web-Server; powershell Add-Content -Path \"C:\\inetpub\\wwwroot\\Default.htm\" -Value $($env:computername)"}'
+
+Get-AzPublicIpAddress -ResourceGroupName AZ-204-IAAS-GRP | Select "IpAddress" | Format-Table
+
+New-Az
